49135a64b03ec2a8fa1099583ab13a23a736b5d9
Fernando Felix do Nascimento Junior <fernandojr.ifcg@live.com>
2016-04-08 13:25:22 -0300

    Enter & Exit

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 // Selections
-d3.selectAll("p").style("color", "white");
-d3.selectAll("body").style("background", "black");
+var selections = function() {
+  d3.selectAll("p").style("color", "white");
+  d3.selectAll("body").style("background", "black");
+}
 
 // Dynamic properties
-d3.selectAll("p").style("color", function() {
-  return "hsl(" + Math.random() * 360 + ",100%,50%)";
-});
+var dynamicProperties = function () {
+  d3.selectAll("p").style("color", function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+  });
 
-d3.selectAll("p").style("color", function(d, i) {
-  return i % 2 ? "#fff" : "#999";
-});
+  d3.selectAll("p").style("color", function(d, i) {
+    return i % 2 ? "#fff" : "#999";
+  });
 
-d3.selectAll("p")
-    .data([4, 8, 15, 16, 23, 42])
-    .style("font-size", function(d) { return d + "px"; });
+  d3.selectAll("p")
+      .data([4, 8, 15, 16, 23, 42])
+      .style("font-size", function(d) { return d + "px"; });
+}
+
+// Enter and Exit
+var enterAndExit = function () {
+  d3.select("body main .container #content").selectAll("p")
+      .data([4, 8, 15, 16, 23, 42])
+    .enter().append("p")
+      .text(function(d) { return "I’m number " + d + "!"; });
+
+  var p = d3.select("body").selectAll("p")
+      .data([4, 8, 15, 16, 23, 42])
+      .text(function(d) { return d; });
+
+  p.enter().append("p")
+      .text(function(d) { return d; });
+
+  p.exit().remove();
+}
