c99874c9ade40e6dc52bf9268cc05f7c2546a8da
Fernando Felix <fernandojr.ifcg@live.com>
2016-04-10 20:54:58 -0300

    Update script.js for circle tutorial.

--- script.js
+++ script.js
@@ -1,64 +1,111 @@
-var data = [4, 8, 15, 16, 23, 42];
+var randomInt = function (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
 
-// Selections
+var sample = function (low, high, length) {
+    var numbers = new Array(length);
+    for (var i = 0; i < length; i++)
+        numbers[i] = randomInt(low, high);
+    return numbers;
+}
+
+var randomColor = function () {
+    return "hsl(" + Math.random() * 360 + ", 100%, 50%)"
+}
+
 var selections = function() {
-  d3.selectAll("p").style("color", "white");
-  d3.selectAll("body").style("background", "black");
+  d3.select("header").style("color", "f0f0f0");
+  d3.selectAll("circle").attr("fill", "blue");
 }
 
-// Dynamic properties
-var dynamicProperties = function () {
-  d3.selectAll("p").style("color", function() {
-    return "hsl(" + Math.random() * 360 + ",100%,50%)";
-  });
-
-  d3.selectAll("p").style("color", function(d, i) {
-    return i % 2 ? "#fff" : "#999";
-  });
-
-  d3.selectAll("p")
-      .data(data)
-      .style("font-size", function(d) { return d + "px"; });
+var clear = function (selector) {
+    d3.select("main").text("");
 }
 
-// Enter and Exit
-var enterAndExit = function () {
-  d3.select("#content").selectAll("p")
-      .data(data)
-    .enter().append("p")
-      .text(function(d) { return "I’m number " + d + "!"; });
-
-  var p = d3.select("body").selectAll("p")
-      .data(data)
-      .text(function(d) { return d; });
-
-  p.enter().append("p")
-      .text(function(d) { return d; });
-
-  p.exit().remove();
+var helloWorld = function () {
+    d3.select("main")
+    .append("div")
+        .attr("class", "container")
+        .text("Hello World");
 }
 
-// Transitions
-var transitions = function () {
-    d3.select("#content").selectAll("circle")
-        .data(data).enter()
-        .append("svg")
-            .append("circle")
-                .attr("cx", 50)
-                .attr("cy", 50)
-                .attr("r", 50)
-                .attr("fill", "red");
+var showData = function (data) {
+    d3.select("footer .container")
+    .text(function() { return "data=[" + data + "];" });
+}
 
-    d3.select("body")
-        .transition()
-        .duration(3000)
-        .style("background-color", "black");
+var createCircles = function (data, radius) {
+    d3.select("main")
+    .selectAll("svg circle")
+    .data(data)
+    .enter()
+    .append("svg")
+        .attr({height: radius * 2, width: radius * 2})
+        .append("circle")
+            .attr({cx: radius, cy: radius, r: radius, fill: "red"});
+}
 
+var changeCircleRadius = function (data) {
     d3.selectAll("circle")
-        .transition()
-        .duration(3000)
-        .delay(function(d, i) { return i * 100; })
-        .attr("r", function(d, i) { return Math.sqrt(d * 5); });
+    .data(data)
+    .attr("r", function(datum) { return datum; });
 }
 
-transitions();
+var changeOddCircleColor = function (data) {
+    d3.selectAll("circle")
+    .data(data)
+    .attr("fill", function(datum, i) {
+        var oldColor = this.getAttribute("fill");
+        var newColor = randomColor();
+        return i % 2 ? newColor : oldColor;
+    });
+}
+
+var changeBackgroundColor = function () {
+    d3.select("main")
+    .transition()
+    .duration(1000)
+    .style("background-color", function () { return randomColor(); });
+}
+
+var cicleTransition = function (duration, length) {
+    d3.selectAll("circle")
+    .transition()
+    .duration(duration)
+    .attr("fill", function() { return randomColor(); })
+    .delay(function() { return randomInt(1, length); })
+    .attr("r", function() { return randomInt(1, length); });
+}
+
+var cicleLoop = function () {
+    var duration = 1000;
+    var repeat = function() {
+        cicleTransition(duration, length);
+        setTimeout(repeat, duration);
+    };
+    repeat();
+}
+
+// selections();
+
+// var data = sample(1, 50, 3);
+// showData(data);
+// changeCircleRadius(data);
+// changeOddCircleColor(data);
+
+// clear();
+// helloWorld();
+
+// clear();
+// var length = randomInt(1, 100);
+// var data = sample(1, 50, length);
+// showData(data);
+// createCircles(data, length);
+
+// changeBackgroundColor();
+
+clear();
+var length = randomInt(1, 100);
+var data = sample(1, 50, length);
+createCircles(data, length);
+cicleLoop();
