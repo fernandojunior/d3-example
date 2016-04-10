7794694ec76711ad83141fd7096c69041078be75
Fernando Felix <fernandojr.ifcg@live.com>
2016-04-10 10:50:54 -0300

    Transitions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var data = [4, 8, 15, 16, 23, 42];
+
 // Selections
 var selections = function() {
   d3.selectAll("p").style("color", "white");
@@ -15,19 +17,19 @@ var dynamicProperties = function () {
   });
 
   d3.selectAll("p")
-      .data([4, 8, 15, 16, 23, 42])
+      .data(data)
       .style("font-size", function(d) { return d + "px"; });
 }
 
 // Enter and Exit
 var enterAndExit = function () {
-  d3.select("body main .container #content").selectAll("p")
-      .data([4, 8, 15, 16, 23, 42])
+  d3.select("#content").selectAll("p")
+      .data(data)
     .enter().append("p")
       .text(function(d) { return "I’m number " + d + "!"; });
 
   var p = d3.select("body").selectAll("p")
-      .data([4, 8, 15, 16, 23, 42])
+      .data(data)
       .text(function(d) { return d; });
 
   p.enter().append("p")
@@ -35,3 +37,28 @@ var enterAndExit = function () {
 
   p.exit().remove();
 }
+
+// Transitions
+var transitions = function () {
+    d3.select("#content").selectAll("circle")
+        .data(data).enter()
+        .append("svg")
+            .append("circle")
+                .attr("cx", 50)
+                .attr("cy", 50)
+                .attr("r", 50)
+                .attr("fill", "red");
+
+    d3.select("body")
+        .transition()
+        .duration(3000)
+        .style("background-color", "black");
+
+    d3.selectAll("circle")
+        .transition()
+        .duration(3000)
+        .delay(function(d, i) { return i * 100; })
+        .attr("r", function(d, i) { return Math.sqrt(d * 5); });
+}
+
+transitions();
