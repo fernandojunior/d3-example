c8962e327804ded5873ec6892f28d8141db7eca6
Fernando Felix <fernandojr.ifcg@live.com>
2016-04-17 17:11:38 -0300

    Hello World example.

---
+++ hello-world/script.js
@@ -0,0 +1,27 @@
+var randomInt = function(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+var color = function(hue) {
+    return "hsl(" + hue + ", 100%, 50%)"
+}
+
+var helloWorld = function(msg) {
+    var random = randomInt(1, 360);
+    d3.select("#hello_world")
+        .style({color: color(random), "font-size": random})
+        .html(function() { return msg + " " + random; });
+}
+
+var loop = function(args) {
+    var duration = 1000/2;
+    var repeat = function() {
+        helloWorld.call(null, args);
+        setTimeout(repeat, duration);
+    };
+    repeat();
+}
+
+var msg = "Rebeca ♥";
+// helloWorld(msg)
+loop(msg);
